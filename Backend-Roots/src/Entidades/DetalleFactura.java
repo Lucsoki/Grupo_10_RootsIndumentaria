bacec369dbe892bfdb924a79892b3c9cc7e58e29
xaetor <38496427+xaetor@users.noreply.github.com>
2022-10-11 18:03:07 -0300

    Actualizacion Factura

--- Backend-Roots/src/Entidades/DetalleFactura.java
+++ Backend-Roots/src/Entidades/DetalleFactura.java
@@ -1,8 +1,6 @@
 package entidades;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
@@ -12,26 +10,24 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import org.hibernate.envers.Audited;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Builder.Default;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Table(name = "factura")
+@Table(name = "detalle_factura")
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 @Getter
 @Setter
-public class Factura implements Serializable {
+public class DetalleFactura implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
@@ -39,23 +35,19 @@ public class Factura implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "fecha")
-    private String fecha;
+    @Column(name = "cantidad")
+    private int cantidad;
 
-    @Column(name = "numero")
-    private int numero;
-
-    @Column(name = "total")
-    private int total;
+    @Column(name = "subtotal")
+    private int subtotal;
 
     @ManyToOne(cascade = CascadeType.PERSIST)
-    @JoinColumn(name = "fk_cliente")
-    private Cliente cliente;
+    @JoinColumn(name = "fk_factura")
+    private Factura factura;
 
-    //@OneToMany(cascade =CascadeType.ALL, orphanRemoval = true)
-    //private List<DetalleFactura> detalles = new ArrayList <DetalleFactura>();
-    @OneToMany(mappedBy = "factura",cascade = CascadeType.ALL, orphanRemoval = true)
-    @Default
-    private List<DetalleFactura> detalle = new ArrayList<DetalleFactura>();
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "fk_articulo")
+    private Articulo articulo;
+
 
 }

0ea8a2be402e6bcc5d37517c3742f170ef439fa7
xaetor <38496427+xaetor@users.noreply.github.com>
2022-10-14 16:11:00 -0300

    -

--- Backend-Roots/src/main/java/com/examples/rootsindumentaria/controllers/BaseControllerImpl.java
+++ Backend-Roots/src/main/java/com/examples/rootsindumentaria/controllers/BaseControllerImpl.java
@@ -1,5 +1,6 @@
 package com.example.rootsindumentaria.controllers;
 
+import com.example.rootsindumentaria.services.BaseServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;

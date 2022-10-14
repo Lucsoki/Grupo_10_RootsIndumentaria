7085ed6616db2781ebbb0ebcb690a970ad8370d8
xaetor <38496427+xaetor@users.noreply.github.com>
2022-10-14 10:27:50 -0300

    Update BaseService.java

--- Backend-Roots/src/main/java/com/examples/rootsindumentaria/services/BaseService.java
+++ Backend-Roots/src/main/java/com/examples/rootsindumentaria/services/BaseService.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 public interface BaseService <E, ID extends Serializable> {
     public List<E> findAll() throws Exception;
-    public Page<E> findAll(Pageable pageable) throws Exception; // este metodo lo vamos a utilizar despues.Por el momento solo lo vamos a dejar.
+    public Page<E> findAll(Pageable pageable) throws Exception; 
     public E findById(ID id) throws Exception;
     public E save(E entity) throws Exception;
     public E update (ID id, E entity) throws Exception;
